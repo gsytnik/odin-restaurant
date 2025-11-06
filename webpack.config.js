8c94efd2e0168d50bd375b177ad8b774edcca7fe
gsytnik <39895377+gsytnik@users.noreply.github.com.>
2025-11-06 14:19:14 -0800

    update config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 // webpack.config.js
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -9,4 +10,17 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template : "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        },
+    ]
+  }
 };
